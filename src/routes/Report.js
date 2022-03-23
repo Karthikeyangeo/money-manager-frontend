b3e74a70e0375423775c5a731f15405a1fb9455a
karthikeyangeo <karthikeyangeo@gmail.com>
2022-03-23 18:45:21 +0530

    table

--- src/routes/Report.js
+++ src/routes/Report.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useState ,useEffect} from 'react';
-import {API} from './global';
+import {API,rows} from './global';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
@@ -19,104 +19,21 @@ const columns = [
     
   ];
   
-  const rows = [
-    {
-      type: "type 1",
-      date: "date 1",
-      desc: "desc 1",
-      division: "division 1",
-      category: "category 1",
-      amount: 1000,
-      id: "1"
-    },
-    {
-      type: "type 2",
-      date: "date 2",
-      desc: "desc 2",
-      division: "division 2",
-      category: "category 2",
-      amount: 9,
-      id: "2"
-    },
-    {
-      type: "type 3",
-      date: "date 3",
-      desc: "desc 3",
-      division: "division 3",
-      category: "category 3",
-      amount: 1,
-      id: "3"
-    },
-    {
-      type: "type 4",
-      date: "date 4",
-      desc: "desc 4",
-      division: "division 4",
-      category: "category 4",
-      amount: 61,
-      id: "4"
-    },
-    {
-      type: "type 5",
-      date: "date 5",
-      desc: "desc 5",
-      division: "division 5",
-      category: "category 5",
-      amount: 27,
-      id: "5"
-    },
-    {
-      type: "type 6",
-      date: "date 6",
-      desc: "desc 6",
-      division: "division 6",
-      category: "category 6",
-      amount: 31,
-      id: "6"
-    },
-    {
-      type: "type 7",
-      date: "date 7",
-      desc: "desc 7",
-      division: "division 7",
-      category: "category 7",
-      amount: 78,
-      id: "7"
-    },
-    {
-      type: "type 8",
-      date: "date 8",
-      desc: "desc 8",
-      division: "division 8",
-      category: "category 8",
-      amount: 63,
-      id: "8"
-    },
-    {
-      type: "type 9",
-      date: "date 9",
-      desc: "desc 9",
-      division: "division 9",
-      category: "category 9",
-      amount: 48,
-      id: "9"
-    },
-    {
-      type: "type 10",
-      date: "date 10",
-      desc: "desc 10",
-      division: "division 10",
-      category: "category 10",
-      amount: 31,
-      id: "10"
-    }
-  ];
+
 
 
 export function Report(){
     
     const [amtData,setAmtData]= useState([]);
 
+    const incomeArray = amtData.filter(x=>x.type ==='Income');
+    const incomeTotal = incomeArray.map(x =>x.amount).reduce((a,b)=>a+b,0);
+    const expenseArray = amtData.filter(x=>x.type ==='Expense');
+    const expenseTotal = expenseArray.map(x =>x.amount).reduce((a,b)=>a+b,0);
+    console.log(`incomeTotal`,incomeTotal)
+    console.log(`income`,incomeArray)
+    console.log(`expense`,expenseArray)
+    console.log(`expensetotal`,expenseTotal)
 
     let getAmt = async () =>{
         await fetch(`${API}/track`,{
@@ -133,17 +50,26 @@ export function Report(){
     useEffect(getAmt,[])
     
     return(
+      <div className='report'>
         <div className='dataTable'>
             <div style={{ height: 400, width: '100%' }}>
             <DataGrid
-            rows={rows}
+            getRowId={(r) => r._id}
+            rows={amtData}
             columns={columns}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
             
             />
             </div>
+        </div>  
+        <div className='sum'>
+          Total sum = {incomeTotal+ expenseTotal}
+          Income Total = {incomeTotal}
+          Expense Total = {expenseTotal}
         </div>
+      </div>
+        
 
     )
 }
--- src/routes/global.js
+++ src/routes/global.js
@@ -1,1 +1,97 @@
-export const API = `https://money-manager-mk.herokuapp.com`;
+const API = `https://money-manager-mk.herokuapp.com`;
+
+// mock data for checking 
+const rows = [
+    {
+      type: "type 1",
+      date: "date 1",
+      desc: "desc 1",
+      division: "division 1",
+      category: "category 1",
+      amount: 1000,
+      id: "1"
+    },
+    {
+      type: "type 2",
+      date: "date 2",
+      desc: "desc 2",
+      division: "division 2",
+      category: "category 2",
+      amount: 9,
+      id: "2"
+    },
+    {
+      type: "type 3",
+      date: "date 3",
+      desc: "desc 3",
+      division: "division 3",
+      category: "category 3",
+      amount: 1,
+      id: "3"
+    },
+    {
+      type: "type 4",
+      date: "date 4",
+      desc: "desc 4",
+      division: "division 4",
+      category: "category 4",
+      amount: 61,
+      id: "4"
+    },
+    {
+      type: "type 5",
+      date: "date 5",
+      desc: "desc 5",
+      division: "division 5",
+      category: "category 5",
+      amount: 27,
+      id: "5"
+    },
+    {
+      type: "type 6",
+      date: "date 6",
+      desc: "desc 6",
+      division: "division 6",
+      category: "category 6",
+      amount: 31,
+      id: "6"
+    },
+    {
+      type: "type 7",
+      date: "date 7",
+      desc: "desc 7",
+      division: "division 7",
+      category: "category 7",
+      amount: 78,
+      id: "7"
+    },
+    {
+      type: "type 8",
+      date: "date 8",
+      desc: "desc 8",
+      division: "division 8",
+      category: "category 8",
+      amount: 63,
+      id: "8"
+    },
+    {
+      type: "type 9",
+      date: "date 9",
+      desc: "desc 9",
+      division: "division 9",
+      category: "category 9",
+      amount: 48,
+      id: "9"
+    },
+    {
+      type: "type 10",
+      date: "date 10",
+      desc: "desc 10",
+      division: "division 10",
+      category: "category 10",
+      amount: 31,
+      id: "10"
+    }
+  ];
+
+  export {API,rows};
